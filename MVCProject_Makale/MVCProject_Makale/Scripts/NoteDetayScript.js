1f1f531aff43c48421472312cd4be7c02cfa953d
betulerkal <96913284+betulerkal@users.noreply.github.com>
2022-06-01 10:15:50 +0300

    Makale

---
+++ MVCProject_Makale/MVCProject_Makale/Scripts/NoteDetayScript.js
@@ -0,0 +1,7 @@
+﻿
+$(function () {
+    $('#ModalNote').on('show.bs.modal', function (e) {
+        var notid = $(e.relatedTarget).data("notid");   //related target butonu yakalamak için
+        $("#modal-body").load("/Note/NotDetay/" + notid);
+    })
+})
